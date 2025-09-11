aa4aab74e0ad0034fe018599ecfa3a53a25d8d06
Edouard Lukau <edouard.lukau@kbcrawl.com>
2025-09-11 18:46:05 +0200

    prom-client + metrics endpoint middleware

--- src/app.js
+++ src/app.js
@@ -19,7 +19,8 @@ app.use(cors({
         process.env.FRONTEND_URL,
         process.env.FRONTEND_URL.replace('http://', 'http://127.0.0.1:'),
         'http://localhost:5500',
-        'http://127.0.0.1:5500'
+        'http://127.0.0.1:5500',
+        "https://scanitynov.grafana.net"
     ].filter(Boolean),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -34,8 +35,17 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(`📩 Requête reçue : ${req.method} ${req.url}`);
+
+    // Middleware pour mesurer chaque requête
+    const end = httpRequestDurationSeconds.startTimer();
+    res.on('finish', () => {
+      httpRequestsTotal.inc({ method: req.method, route: req.path, status: res.statusCode });
+      end({ method: req.method, route: req.path, status: res.statusCode });
+    });
+
     next();
 });
+
 
 // 🚏 Importer les routes
 const userRoutes = require('./routes/user.routes');
@@ -54,4 +64,45 @@ app.use('/api/applications', applicationRoutes);
 app.get("/api/ping", (req, res) => {
     res.json({message :"✅ Serveur Express fonctionne !"});
 });
+
+//----------------partie metrics------------//
+const client = require('prom-client');
+
+// Crée un registre pour stocker toutes les métriques
+const register = new client.Registry();
+
+// Ajoute des métriques par défaut (CPU, mémoire, etc.)
+client.collectDefaultMetrics({ register });
+
+// Exemple : compteur personnalisé
+const httpRequestsTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Nombre total de requêtes HTTP reçues',
+  labelNames: ['method', 'route', 'status']
+});
+
+// Exemple : histogramme pour les temps de réponse
+const httpRequestDurationSeconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Durée des requêtes HTTP en secondes',
+  labelNames: ['method', 'route', 'status'],
+  buckets: [0.1, 0.5, 1, 2, 5]
+});
+
+// Enregistre les métriques
+register.registerMetric(httpRequestsTotal);
+register.registerMetric(httpRequestDurationSeconds);
+
+
+// Endpoint pour exposer les métriques
+app.get('/api/metrics', async (req, res) => {
+
+  //middleware
+    if (req.query.token !== process.env.METRICS_TOKEN) return res.status(403).send("Forbidden");
+    
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
+
 module.exports = app;
